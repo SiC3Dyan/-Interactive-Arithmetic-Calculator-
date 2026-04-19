325235f484d87f970f7416af5391f491b67d19ff
rcedric_246 <rcedric.cruz@gmail.com>
2026-04-19 15:45:53 +0800

    Update Seat6.cpp

--- Seat6.cpp
+++ Seat6.cpp
@@ -19,11 +19,14 @@ int main () {
 
     switch(op) {
       case: "+"
-        result = num1/num2;
+        result = num1 + num2;
       cout << "Result: " << num1 << "+" << num2 << result << endl;
       break;
 
-      
+      case: "-"
+        result = num1 - num2;
+      cout << "Result: " << num1 << "-" << num2 << results << endl;
+      break;
     }
   }
 }
